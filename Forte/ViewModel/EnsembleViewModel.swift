d8761ae3fab4dccf036ce74e0eaccbf08519ed0e
Marthen Nodado <mdrnodado@gmail.com>
2023-12-27 14:17:32 -0800

    fix: deleting groups thru viewmodel

--- Forte/ViewModel/EnsembleViewModel.swift
+++ Forte/ViewModel/EnsembleViewModel.swift
@@ -10,21 +10,21 @@ import CoreData
 import SwiftUI
 
 class EnsembleViewModel: ObservableObject {
-    @Published var groups: [Ensemble] = []
+//    @Published var groups: [Ensemble] = []
     
     func createEnsemble(for name: String) {
         let _ = DataManager.shared.ensemble(name: name)
         DataManager.shared.save()
     }
     
-    func loadEnsembleList() {
-        self.groups = DataManager.shared.ensembles()
+    internal func loadEnsembleList() -> [Ensemble] {
+        return DataManager.shared.ensembles()
     }
     
     // ? - why delete from a set of indices than one index?
     func removeEnsemble(at offsets: IndexSet) {
         // TODO: add a prompt to ensure desired item deletion
-        
+        let groups = loadEnsembleList() // is this even necessary
         for index in offsets {
             let group = groups[index]
             DataManager.shared.deleteEnsemble(ensemble: group)
